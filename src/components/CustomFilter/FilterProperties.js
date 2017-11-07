d4b917c1d27e5a5132265c5fb44a215a516a5fe3
Raphael Fernandes <raphaelfp.eng@gmail.com>
2017-11-06 23:41:23 -0200

    dynamic child problem

--- src/components/CustomFilter/FilterProperties.js
+++ src/components/CustomFilter/FilterProperties.js
@@ -1,15 +1,20 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import { withRouter } from 'react-router-dom';
 import { writeImageData } from '../../lib/web-dsp/WebDSP';
-import { Grid, Checkbox } from 'semantic-ui-react';
+import { Grid, Checkbox, Form } from 'semantic-ui-react';
 //import Jimp from 'jimp';
 /* global Jimp */
 
 class FilterProperties extends React.Component {
 	state = {
-		jimp: {}
+		conv: {
+			selected: false,
+			size: 0,
+			mask: []
+		}
 	};
 
 	componentDidMount() {
@@ -46,74 +51,100 @@ class FilterProperties extends React.Component {
 		);
 	}
 
-	applyJimpConvolution(kernel) {
-		const canvas = document.getElementById('image-canvas');
-
-		canvas.toBlob(
-			async blob => {
-				const url = URL.createObjectURL(blob);
-				const image = await Jimp.read(url);
-				debugger;
-				image.convolution(kernel);
-				writeImageData(
-					canvas,
-					image.bitmap.data,
-					image.bitmap.width,
-					image.bitmap.height
-				);
-				URL.revokeObjectURL(url);
-			},
-			'image/png',
-			1
+	renderCheckbox(label, filter, ...params) {
+		return (
+			<Checkbox
+				toggle
+				label={label}
+				onClick={() => this.applyJimpFilter(filter, ...params)}
+			/>
 		);
 	}
+
+	handleChangeMatrixRadio = (e, { value }) => {
+		let matrix;
+		switch (value) {
+			case 3:
+				matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+				break;
+			case 5:
+				matrix = [
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0]
+				];
+				break;
+			case 7:
+				matrix = [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0]
+				];
+				break;
+			default:
+				break;
+		}
+		this.setState({
+			...this.state,
+			conv: {
+				selected: true,
+				size: value,
+				mask: matrix
+			}
+		});
+	};
+
+	renderConvolutionInputs = () => {
+		let ret = [];
+
+		for (var x = 0; x < this.state.conv.size; x++) {
+			ret.push(<Form.Group />);
+			debugger;
+			ret[x].push(<Form.Group inline />);
+			for (var y = 0; y < this.state.conv.size; y++) {
+				ret[x].push(<Form.Input value={this.state.conv.mask[x][y]} />);
+			}
+		}
+
+		return ret;
+	};
 
 	render() {
 		return (
 			<Grid>
 				<Grid.Row>
 					<Grid.Column>
-						<Checkbox
-							toggle
-							label="GreyScale"
-							onClick={() => this.applyJimpFilter('greyscale')}
-						/>
-						<Checkbox
-							toggle
-							label="Dither 565"
-							onClick={() => this.applyJimpFilter('dither565')}
-						/>
-						<Checkbox
-							toggle
-							label="Normalize"
-							onClick={() => this.applyJimpFilter('normalize')}
-						/>
-						<Checkbox
-							toggle
-							label="Rotate"
-							onClick={() => this.applyJimpFilter('rotate', 90)}
-						/>
-						<Checkbox
-							toggle
-							label="Resize"
-							onClick={() => this.applyJimpFilter('resize', 100, 100)}
-						/>
-						<Checkbox
-							toggle
-							label="Convolute"
-							onClick={() =>
-								this.applyJimpConvolution([
-									[1, 0, 0, 0, 0, 0, 0, 0, 0],
-									[0, 1, 0, 0, 0, 0, 0, 0, 0],
-									[0, 0, 1, 0, 0, 0, 0, 0, 0],
-									[0, 0, 0, 1, 0, 0, 0, 0, 0],
-									[0, 0, 0, 0, 1, 0, 0, 0, 0],
-									[0, 0, 0, 0, 0, 1, 0, 0, 0],
-									[0, 0, 0, 0, 0, 0, 1, 0, 0],
-									[0, 0, 0, 0, 0, 0, 0, 1, 0],
-									[0, 0, 0, 0, 0, 0, 0, 0, 1]
-								])}
-						/>
+						<Form>
+							<Form.Field>Matrix de Convolução</Form.Field>
+							<Form.Group inline>
+								<label>Tamanho:</label>
+								<Form.Radio
+									label="3x3"
+									value={3}
+									checked={this.state.conv.size === 3}
+									onChange={this.handleChangeMatrixRadio}
+								/>
+								<Form.Radio
+									label="5x5"
+									value={5}
+									checked={this.state.conv.size === 5}
+									onChange={this.handleChangeMatrixRadio}
+								/>
+								<Form.Radio
+									label="7x7"
+									value={7}
+									checked={this.state.conv.size === 7}
+									onChange={this.handleChangeMatrixRadio}
+								/>
+							</Form.Group>
+							{this.renderConvolutionInputs()}
+						</Form>
 					</Grid.Column>
 				</Grid.Row>
 			</Grid>
