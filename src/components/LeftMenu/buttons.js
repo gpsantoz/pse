1a64f60c26ee1471427db7672dbdd932cf031a27
Josué Santos <josuesasilva@gmail.com>
2018-05-22 21:34:30 -0300

    Adiciona opções das novas funcionalidades no menu

--- src/components/LeftMenu/buttons.js
+++ src/components/LeftMenu/buttons.js
@@ -1,146 +1,158 @@
 import { CUSTOM_FILTER } from '../../actions/types';
 
 export default [
-	{
-		label: 'Interface',
-		buttons: [
-			{
-				color: 'blue',
-				label: 'Abrir imagem'
-			}
-		]
-	},
-	{
-		label: 'Processamento',
-		buttons: [
-			{
-				color: 'green',
-				label: CUSTOM_FILTER
-			},
-			{
-				color: 'yellow',
-				label: 'Grayscale'
-			},
-			{
-				color: 'yellow',
-				label: 'Brighten'
-			},
-			{
-				color: 'yellow',
-				label: 'Invert'
-			},
-			{
-				color: 'yellow',
-				label: 'Noise'
-			},
-			{
-				color: 'yellow',
-				label: 'Sunset'
-			},
-			{
-				color: 'yellow',
-				label: 'Analog'
-			},
-			{
-				color: 'yellow',
-				label: 'Emboss'
-			},
-			{
-				color: 'yellow',
-				label: 'Sobel'
-			},
-			{
-				color: 'yellow',
-				label: 'Sobel Invertido'
-			},
-			{
-				color: 'yellow',
-				label: 'Gaussian Blur'
-			},
-			{
-				color: 'yellow',
-				label: 'Sharpen'
-			},
-			{
-				color: 'yellow',
-				label: 'Uber Sharpen'
-			},
-			{
-				color: 'yellow',
-				label: 'Urple'
-			},
-			{
-				color: 'yellow',
-				label: 'Forest'
-			},
-			{
-				color: 'yellow',
-				label: 'Romance'
-			},
-			{
-				color: 'yellow',
-				label: 'Hippo'
-			},
-			{
-				color: 'yellow',
-				label: 'Longhorn'
-			},
-			{
-				color: 'yellow',
-				label: 'Underground'
-			},
-			{
-				color: 'yellow',
-				label: 'Rooster'
-			},
-			{
-				color: 'yellow',
-				label: 'Mist'
-			},
-			{
-				color: 'yellow',
-				label: 'Kaleidoscope'
-			},
-			{
-				color: 'yellow',
-				label: 'Bacteria'
-			},
-			{
-				color: 'yellow',
-				label: 'Clarity'
-			},
-			{
-				color: 'yellow',
-				label: 'Good Morning'
-			},
-			{
-				color: 'yellow',
-				label: 'Acid'
-			},
-			{
-				color: 'yellow',
-				label: 'Dewdrops'
-			},
-			{
-				color: 'yellow',
-				label: 'Color Destruction'
-			},
-			{
-				color: 'yellow',
-				label: 'Hulk Edge'
-			},
-			{
-				color: 'yellow',
-				label: 'Ghost'
-			},
-			{
-				color: 'yellow',
-				label: 'Twisted'
-			},
-			{
-				color: 'yellow',
-				label: 'Security'
-			}
-		]
-	}
+  {
+    label: 'Interface',
+    buttons: [
+      {
+        color: 'blue',
+        label: 'Abrir imagem',
+      },
+    ],
+  },
+  {
+    label: 'Processamento',
+    buttons: [
+      {
+        color: 'orange',
+        label: 'Interpolação pelo vizinho mais próximo',
+      },
+      {
+        color: 'orange',
+        label: 'Interpolação Bicúbica',
+      },
+      {
+        color: 'orange',
+        label: 'Interpolação Bilinear',
+      },
+      {
+        color: 'green',
+        label: CUSTOM_FILTER,
+      },
+      {
+        color: 'yellow',
+        label: 'Grayscale',
+      },
+      {
+        color: 'yellow',
+        label: 'Brighten',
+      },
+      {
+        color: 'yellow',
+        label: 'Invert',
+      },
+      {
+        color: 'yellow',
+        label: 'Noise',
+      },
+      {
+        color: 'yellow',
+        label: 'Sunset',
+      },
+      {
+        color: 'yellow',
+        label: 'Analog',
+      },
+      {
+        color: 'yellow',
+        label: 'Emboss',
+      },
+      {
+        color: 'yellow',
+        label: 'Sobel',
+      },
+      {
+        color: 'yellow',
+        label: 'Sobel Invertido',
+      },
+      {
+        color: 'yellow',
+        label: 'Gaussian Blur',
+      },
+      {
+        color: 'yellow',
+        label: 'Sharpen',
+      },
+      {
+        color: 'yellow',
+        label: 'Uber Sharpen',
+      },
+      {
+        color: 'yellow',
+        label: 'Urple',
+      },
+      {
+        color: 'yellow',
+        label: 'Forest',
+      },
+      {
+        color: 'yellow',
+        label: 'Romance',
+      },
+      {
+        color: 'yellow',
+        label: 'Hippo',
+      },
+      {
+        color: 'yellow',
+        label: 'Longhorn',
+      },
+      {
+        color: 'yellow',
+        label: 'Underground',
+      },
+      {
+        color: 'yellow',
+        label: 'Rooster',
+      },
+      {
+        color: 'yellow',
+        label: 'Mist',
+      },
+      {
+        color: 'yellow',
+        label: 'Kaleidoscope',
+      },
+      {
+        color: 'yellow',
+        label: 'Bacteria',
+      },
+      {
+        color: 'yellow',
+        label: 'Clarity',
+      },
+      {
+        color: 'yellow',
+        label: 'Good Morning',
+      },
+      {
+        color: 'yellow',
+        label: 'Acid',
+      },
+      {
+        color: 'yellow',
+        label: 'Dewdrops',
+      },
+      {
+        color: 'yellow',
+        label: 'Color Destruction',
+      },
+      {
+        color: 'yellow',
+        label: 'Hulk Edge',
+      },
+      {
+        color: 'yellow',
+        label: 'Ghost',
+      },
+      {
+        color: 'yellow',
+        label: 'Twisted',
+      },
+      {
+        color: 'yellow',
+        label: 'Security',
+      },
+    ],
+  },
 ];
--- src/components/shared/handleFilter.js
+++ src/components/shared/handleFilter.js
@@ -2,102 +2,102 @@ import CoreDSP from '../../lib/web-dsp/CoreDSP';
 const coreDSP = new CoreDSP();
 
 export const handleFilter = (filter, pixels) => {
-	const { width, height } = pixels;
-	switch (filter) {
-		case 'grayscale':
-			pixels.data.set(coreDSP.grayscale(pixels.data));
-			break;
-		case 'brighten':
-			pixels.data.set(coreDSP.brighten(pixels.data));
-			break;
-		case 'invert':
-			pixels.data.set(coreDSP.invert(pixels.data));
-			break;
-		case 'noise':
-			pixels.data.set(coreDSP.noise(pixels.data));
-			break;
-		case 'sunset':
-			pixels.data.set(coreDSP.sunset(pixels.data, width));
-			break;
-		case 'analog':
-			pixels.data.set(coreDSP.analog(pixels.data, width));
-			break;
-		case 'emboss':
-			pixels.data.set(coreDSP.emboss(pixels.data, width));
-			break;
-		case 'sobel':
-			pixels.data.set(coreDSP.sobel(pixels.data, width, height));
-			break;
-		case 'sobel_invertido':
-			pixels.data.set(coreDSP.sobel(pixels.data, width, height, true));
-			break;
-		case 'gaussian_blur':
-			pixels.data.set(coreDSP.blur(pixels.data, width, height));
-			break;
-		case 'sharpen':
-			pixels.data.set(coreDSP.sharpen(pixels.data, width, height));
-			break;
-		case 'uber_sharpen':
-			pixels.data.set(coreDSP.strong_sharpen(pixels.data, width, height));
-			break;
-		case 'clarity':
-			pixels.data.set(coreDSP.clarity(pixels.data, width, height));
-			break;
-		case 'good_morning':
-			pixels.data.set(coreDSP.good_morning(pixels.data, width, height));
-			break;
-		case 'acid':
-			pixels.data.set(coreDSP.acid(pixels.data, width, height));
-			break;
-		case 'urple':
-			pixels.data.set(coreDSP.urple(pixels.data, width));
-			break;
-		case 'forest':
-			pixels.data.set(coreDSP.forest(pixels.data, width));
-			break;
-		case 'romance':
-			pixels.data.set(coreDSP.romance(pixels.data, width));
-			break;
-		case 'hippo':
-			pixels.data.set(coreDSP.hippo(pixels.data, width));
-			break;
-		case 'longhorn':
-			pixels.data.set(coreDSP.longhorn(pixels.data, width));
-			break;
-		case 'underground':
-			pixels.data.set(coreDSP.underground(pixels.data, width));
-			break;
-		case 'rooster':
-			pixels.data.set(coreDSP.rooster(pixels.data, width));
-			break;
-		case 'mist':
-			pixels.data.set(coreDSP.mist(pixels.data, width));
-			break;
-		case 'kaleidoscope':
-			pixels.data.set(coreDSP.kaleidoscope(pixels.data, width));
-			break;
-		case 'bacteria':
-			pixels.data.set(coreDSP.bacteria(pixels.data, width));
-			break;
-		case 'dewdrops':
-			pixels.data.set(coreDSP.dewdrops(pixels.data, width, height));
-			break;
-		case 'color_destruction':
-			pixels.data.set(coreDSP.destruction(pixels.data, width, height));
-			break;
-		case 'hulk_edge':
-			pixels.data.set(coreDSP.hulk(pixels.data, width));
-			break;
-		case 'ghost':
-			pixels.data.set(coreDSP.ghost(pixels.data, width));
-			break;
-		case 'twisted':
-			pixels.data.set(coreDSP.twisted(pixels.data, width));
-			break;
-		case 'security':
-			pixels.data.set(coreDSP.security(pixels.data, width));
-			break;
-		default:
-			break;
-	}
+  const { width, height } = pixels;
+  switch (filter) {
+    case 'grayscale':
+      pixels.data.set(coreDSP.grayscale(pixels.data));
+      break;
+    case 'brighten':
+      pixels.data.set(coreDSP.brighten(pixels.data));
+      break;
+    case 'invert':
+      pixels.data.set(coreDSP.invert(pixels.data));
+      break;
+    case 'noise':
+      pixels.data.set(coreDSP.noise(pixels.data));
+      break;
+    case 'sunset':
+      pixels.data.set(coreDSP.sunset(pixels.data, width));
+      break;
+    case 'analog':
+      pixels.data.set(coreDSP.analog(pixels.data, width));
+      break;
+    case 'emboss':
+      pixels.data.set(coreDSP.emboss(pixels.data, width));
+      break;
+    case 'sobel':
+      pixels.data.set(coreDSP.sobel(pixels.data, width, height));
+      break;
+    case 'sobel_invertido':
+      pixels.data.set(coreDSP.sobel(pixels.data, width, height, true));
+      break;
+    case 'gaussian_blur':
+      pixels.data.set(coreDSP.blur(pixels.data, width, height));
+      break;
+    case 'sharpen':
+      pixels.data.set(coreDSP.sharpen(pixels.data, width, height));
+      break;
+    case 'uber_sharpen':
+      pixels.data.set(coreDSP.strong_sharpen(pixels.data, width, height));
+      break;
+    case 'clarity':
+      pixels.data.set(coreDSP.clarity(pixels.data, width, height));
+      break;
+    case 'good_morning':
+      pixels.data.set(coreDSP.good_morning(pixels.data, width, height));
+      break;
+    case 'acid':
+      pixels.data.set(coreDSP.acid(pixels.data, width, height));
+      break;
+    case 'urple':
+      pixels.data.set(coreDSP.urple(pixels.data, width));
+      break;
+    case 'forest':
+      pixels.data.set(coreDSP.forest(pixels.data, width));
+      break;
+    case 'romance':
+      pixels.data.set(coreDSP.romance(pixels.data, width));
+      break;
+    case 'hippo':
+      pixels.data.set(coreDSP.hippo(pixels.data, width));
+      break;
+    case 'longhorn':
+      pixels.data.set(coreDSP.longhorn(pixels.data, width));
+      break;
+    case 'underground':
+      pixels.data.set(coreDSP.underground(pixels.data, width));
+      break;
+    case 'rooster':
+      pixels.data.set(coreDSP.rooster(pixels.data, width));
+      break;
+    case 'mist':
+      pixels.data.set(coreDSP.mist(pixels.data, width));
+      break;
+    case 'kaleidoscope':
+      pixels.data.set(coreDSP.kaleidoscope(pixels.data, width));
+      break;
+    case 'bacteria':
+      pixels.data.set(coreDSP.bacteria(pixels.data, width));
+      break;
+    case 'dewdrops':
+      pixels.data.set(coreDSP.dewdrops(pixels.data, width, height));
+      break;
+    case 'color_destruction':
+      pixels.data.set(coreDSP.destruction(pixels.data, width, height));
+      break;
+    case 'hulk_edge':
+      pixels.data.set(coreDSP.hulk(pixels.data, width));
+      break;
+    case 'ghost':
+      pixels.data.set(coreDSP.ghost(pixels.data, width));
+      break;
+    case 'twisted':
+      pixels.data.set(coreDSP.twisted(pixels.data, width));
+      break;
+    case 'security':
+      pixels.data.set(coreDSP.security(pixels.data, width));
+      break;
+    default:
+      break;
+  }
 };
